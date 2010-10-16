6505ffc3c874bca0876a903ed865e5b4472d9a0b
userdelroot <r00t316@gmail.com>
2010-10-16 07:52:36 -0400

    removed some logging and fixed some misc stuff.  formatting

--- src/fac/userdelroot/droidprofiles/Profiles.java
+++ src/fac/userdelroot/droidprofiles/Profiles.java
@@ -16,7 +16,6 @@
  * 
  */
 
-
 package fac.userdelroot.droidprofiles;
 
 import java.util.HashMap;
@@ -33,196 +32,187 @@ import android.net.Uri;
  */
 public class Profiles {
 
-	public static final String PROFILE_ID = "_id";
-	public static final String TAG = "Profiles";
+    public static final String PROFILE_ID = "_id";
 
-	/**
-	 * Gets all profiles
-	 * 
-	 * @param cr
-	 *            ContentResolver
-	 * @param omitId
-	 *            Long omitId id to omit from result (optional)
-	 * @return Cursor
-	 */
-	public static Cursor getProfiles(ContentResolver cr, long omitId) {
+    public static final String TAG = "Profiles";
 
-		String selection = null;
-		if (omitId > 0) {
-			selection = "_id !=" + omitId;
-		}
-		Cursor cursor = cr.query(Profile.Columns.CONTENT_URI,
-				Profile.Columns.PROFILE_QUERY, selection, null, null);
-		return cursor;
-	}
+    /**
+     * Gets all profiles
+     * 
+     * @param cr ContentResolver
+     * @param omitId Long omitId id to omit from result (optional)
+     * @return Cursor
+     */
+    public static Cursor getProfiles(ContentResolver cr, long omitId) {
 
-	public static Profile getProfileByID(ContentResolver cr, int id) {
+        String selection = null;
+        if (omitId > 0) {
+            selection = "_id !=" + omitId;
+        }
+        Cursor cursor = cr.query(Profile.Columns.CONTENT_URI, Profile.Columns.PROFILE_QUERY,
+                selection, null, null);
+        return cursor;
+    }
 
-		Cursor cursor = cr.query(ContentUris.withAppendedId(
-				Profile.Columns.CONTENT_URI, id),
-				Profile.Columns.PROFILE_QUERY, null, null, null);
-		Profile profile = null;
-		if (cursor != null) {
-			if (cursor.moveToFirst()) {
-				profile = new Profile(cursor);
-			}
-			cursor.close();
-		}
+    public static Profile getProfileByID(ContentResolver cr, int id) {
 
-		return profile;
-	}
+        Cursor cursor = cr.query(ContentUris.withAppendedId(Profile.Columns.CONTENT_URI, id),
+                Profile.Columns.PROFILE_QUERY, null, null, null);
+        Profile profile = null;
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                profile = new Profile(cursor);
+            }
+            cursor.close();
+        }
 
-	/**
-	 * Get the notification type by the profileId and type
-	 * 
-	 * @param cr
-	 * @param profileId
-	 * @param type
-	 * @return Notify
-	 */
-	public static Notify getNotifyByProfileId(ContentResolver cr, int profileId, int type) {
-		
-		Uri uri = Notify.Columns.CONTENT_URI;
-		Cursor cursor = null;
-		Notify notify = null;
-		Log.e(TAG + "profileId " +profileId + " type " + type);
-		switch(type) {
-		case Notify.Columns.NOTIFY_TYPE_PHONE:
-			cursor = cr.query(Uri.withAppendedPath(uri, "phone/" + profileId), Notify.Columns.NOTIFY_COLUMNS, null, null, null);
-			
-			if (cursor != null) {
-				if (cursor.moveToFirst()) {
-					notify = new Notify(cursor);
-				}
-				cursor.close();
-			}
-			return notify;
-		case Notify.Columns.NOTIFY_TYPE_SMS:
-cursor = cr.query(Uri.withAppendedPath(uri, "sms/" + profileId), Notify.Columns.NOTIFY_COLUMNS, null, null, null);
-			
-			if (cursor != null) {
-				if (cursor.moveToFirst()) {
-					notify = new Notify(cursor);
-				}
-				cursor.close();
-			}
-			return notify;
-		case Notify.Columns.NOTIFY_TYPE_MMS:
-cursor = cr.query(Uri.withAppendedPath(uri, "mms/" + profileId), Notify.Columns.NOTIFY_COLUMNS, null, null, null);
-			
-			if (cursor != null) {
-				if (cursor.moveToFirst()) {
-					notify = new Notify(cursor);
-				}
-				cursor.close();
-			}
-			return notify;			
-		case Notify.Columns.NOTIFY_TYPE_EMAIL:
-cursor = cr.query(Uri.withAppendedPath(uri, "email/" + profileId), Notify.Columns.NOTIFY_COLUMNS, null, null, null);
-			
-			if (cursor != null) {
-				if (cursor.moveToFirst()) {
-					notify = new Notify(cursor);
-				}
-				cursor.close();
-			}
-			return notify;
-			
-		default: 
-		    if (Log.LOGV)
-		        Log.e(TAG + " Unknown notify type " + type);
-			return null;
-		}					
-	}
+        return profile;
+    }
 
-	// TODO: fixme -
-	// this actually is supposed to be Profile.*
-	// AddProfile changes the values in Profile and update will grab those new
-	// values
+    /**
+     * Get the notification type by the profileId and type
+     * 
+     * @param cr
+     * @param profileId
+     * @param type
+     * @return Notify
+     */
+    public static Notify getNotifyByProfileId(ContentResolver cr, int profileId, int type) {
 
-	@SuppressWarnings("unchecked")
-	public static int saveProfile(ContentResolver resolver,
-			HashMap<String, Comparable> profile) {
-		ContentValues values = new ContentValues(profile.size());
-		int id = Integer.parseInt(profile.get(Profile.Columns._ID).toString());
+        Uri uri = Notify.Columns.CONTENT_URI;
+        Cursor cursor = null;
+        Notify notify = null;
+        switch (type) {
+            case Notify.Columns.NOTIFY_TYPE_PHONE:
+                cursor = cr.query(Uri.withAppendedPath(uri, "phone/" + profileId),
+                        Notify.Columns.NOTIFY_COLUMNS, null, null, null);
 
-		if (Log.LOGV)
-			Log.i("Profiles.java->saveProfile->profile " + profile.toString());
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        notify = new Notify(cursor);
+                    }
+                    cursor.close();
+                }
+                return notify;
+            case Notify.Columns.NOTIFY_TYPE_SMS:
+                cursor = cr.query(Uri.withAppendedPath(uri, "sms/" + profileId),
+                        Notify.Columns.NOTIFY_COLUMNS, null, null, null);
 
-		values.put(Profile.Columns.NAME, profile.get(Profile.Columns.NAME)
-				.toString());
-		values.put(Profile.Columns.ACTIVE, profile.get(Profile.Columns.ACTIVE)
-				.toString());
-		values.put(Profile.Columns.SILENT, profile.get(Profile.Columns.SILENT)
-				.toString());
-		values.put(Profile.Columns.VIBRATE, profile
-				.get(Profile.Columns.VIBRATE).toString());
-		values.put(Profile.Columns.RINGER, profile.get(Profile.Columns.RINGER)
-				.toString());
-		values.put(Profile.Columns.RINGTONE, profile.get(
-				Profile.Columns.RINGTONE).toString());
-		values.put(Profile.Columns.RING_VOL, profile.get(
-				Profile.Columns.RING_VOL).toString());
-		values.put(Profile.Columns.OVERRIDES, profile.get(
-				Profile.Columns.OVERRIDES).toString());
-		values.put(Profile.Columns.CUSTOM_NOTIFY, profile.get(
-				Profile.Columns.CUSTOM_NOTIFY).toString());
-		profile.clear();
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        notify = new Notify(cursor);
+                    }
+                    cursor.close();
+                }
+                return notify;
+            case Notify.Columns.NOTIFY_TYPE_MMS:
+                cursor = cr.query(Uri.withAppendedPath(uri, "mms/" + profileId),
+                        Notify.Columns.NOTIFY_COLUMNS, null, null, null);
 
-		if (id > 0) {
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        notify = new Notify(cursor);
+                    }
+                    cursor.close();
+                }
+                return notify;
+            case Notify.Columns.NOTIFY_TYPE_EMAIL:
+                cursor = cr.query(Uri.withAppendedPath(uri, "email/" + profileId),
+                        Notify.Columns.NOTIFY_COLUMNS, null, null, null);
 
-			return resolver.update(ContentUris.withAppendedId(
-					Profile.Columns.CONTENT_URI, id), values, null, null);
-		}
+                if (cursor != null) {
+                    if (cursor.moveToFirst()) {
+                        notify = new Notify(cursor);
+                    }
+                    cursor.close();
+                }
+                return notify;
 
-		Uri newUri = resolver.insert(Profile.Columns.CONTENT_URI, values);
+            default:
+                if (Log.LOGV)
+                    Log.e(TAG + " Unknown notify type " + type);
+                return null;
+        }
+    }
 
-		return Integer.valueOf(newUri.getPathSegments().get(1));
-	}
+    // TODO: fixme -
+    // this actually is supposed to be Profile.*
+    // AddProfile changes the values in Profile and update will grab those new
+    // values
 
-	/**
-	 * Contacts
-	 */
-	public static void deleteContactsbyProfileId(ContentResolver cr,
-			long profileId) {
-		cr.delete(Uri.withAppendedPath(Contacts.Columns.CONTENT_URI,
-				"profiles/" + profileId), null, null);
-	}
+    @SuppressWarnings("unchecked")
+    public static int saveProfile(ContentResolver resolver, HashMap<String, Comparable> profile) {
+        ContentValues values = new ContentValues(profile.size());
+        int id = Integer.parseInt(profile.get(Profile.Columns._ID).toString());
 
-	public static void delContactsByRealId(ContentResolver cr, long real_id) {
-		int numRows = 0;
+        if (Log.LOGV)
+            Log.i(TAG + "->saveProfile->profile " + profile.toString());
 
-		numRows = cr.delete(Uri.withAppendedPath(Contacts.Columns.CONTENT_URI,
-				"realid/" + real_id), null, null);
+        values.put(Profile.Columns.NAME, profile.get(Profile.Columns.NAME).toString());
+        values.put(Profile.Columns.ACTIVE, profile.get(Profile.Columns.ACTIVE).toString());
+        values.put(Profile.Columns.SILENT, profile.get(Profile.Columns.SILENT).toString());
+        values.put(Profile.Columns.VIBRATE, profile.get(Profile.Columns.VIBRATE).toString());
+        values.put(Profile.Columns.RINGER, profile.get(Profile.Columns.RINGER).toString());
+        values.put(Profile.Columns.RINGTONE, profile.get(Profile.Columns.RINGTONE).toString());
+        values.put(Profile.Columns.RING_VOL, profile.get(Profile.Columns.RING_VOL).toString());
+        values.put(Profile.Columns.OVERRIDES, profile.get(Profile.Columns.OVERRIDES).toString());
+        values.put(Profile.Columns.CUSTOM_NOTIFY, profile.get(Profile.Columns.CUSTOM_NOTIFY)
+                .toString());
+        profile.clear();
 
-		if (Log.LOGV)
-			Log.i(TAG + " delContactsByReadId number of rows affected "
-					+ numRows + " for id " + real_id);
-	}
+        if (id > 0) {
 
-	public static void insertContacts(ContentResolver cr, long profileId,
-			long contact_id) {
-		ContentValues values = new ContentValues();
-		values.put(Contacts.Columns.PROFILE_ID, profileId);
-		values.put(Contacts.Columns.REAL_ID, contact_id);
-		values.put(Contacts.Columns.EMAIL, "none");
-		values.put(Contacts.Columns.NAME, "none");
-		values.put(Contacts.Columns.PHONE, "none");
-		cr.insert(Contacts.Columns.CONTENT_URI, values);
-	}
+            return resolver.update(ContentUris.withAppendedId(Profile.Columns.CONTENT_URI, id),
+                    values, null, null);
+        }
 
-	public static Cursor getContacts(ContentResolver cr, long profileId) {
-		// invalid profile id
-		if (profileId > 0) {
-			// Uri uri = Overrides.Columns.CONTENT_URI;
-			Cursor cursor = cr.query(Uri.withAppendedPath(
-					Contacts.Columns.CONTENT_URI, "profiles/" + profileId),
-					Contacts.Columns.CONTACTS_COLUMNS, null, null, null);
+        Uri newUri = resolver.insert(Profile.Columns.CONTENT_URI, values);
 
-			return cursor;
+        return Integer.valueOf(newUri.getPathSegments().get(1));
+    }
 
-		}
-		return null;
+    /**
+     * Contacts
+     */
+    public static void deleteContactsbyProfileId(ContentResolver cr, long profileId) {
+        cr.delete(Uri.withAppendedPath(Contacts.Columns.CONTENT_URI, "profiles/" + profileId),
+                null, null);
+    }
 
-	}
+    public static void delContactsByRealId(ContentResolver cr, long real_id) {
+        int numRows = 0;
+
+        numRows = cr
+                .delete(Uri.withAppendedPath(Contacts.Columns.CONTENT_URI, "realid/" + real_id),
+                        null, null);
+
+        if (Log.LOGV)
+            Log.i(TAG + "->delContactsByReadId number of rows affected " + numRows + " for id "
+                    + real_id);
+    }
+
+    public static void insertContacts(ContentResolver cr, long profileId, long contact_id) {
+        ContentValues values = new ContentValues();
+        values.put(Contacts.Columns.PROFILE_ID, profileId);
+        values.put(Contacts.Columns.REAL_ID, contact_id);
+        values.put(Contacts.Columns.EMAIL, "none");
+        values.put(Contacts.Columns.NAME, "none");
+        values.put(Contacts.Columns.PHONE, "none");
+        cr.insert(Contacts.Columns.CONTENT_URI, values);
+    }
+
+    public static Cursor getContacts(ContentResolver cr, long profileId) {
+        // invalid profile id
+        if (profileId > 0) {
+            // Uri uri = Overrides.Columns.CONTENT_URI;
+            Cursor cursor = cr.query(
+                    Uri.withAppendedPath(Contacts.Columns.CONTENT_URI, "profiles/" + profileId),
+                    Contacts.Columns.CONTACTS_COLUMNS, null, null, null);
+
+            return cursor;
+
+        }
+        return null;
+
+    }
 }
