5e2588cb13e8dffb0478b9d0e6c5a73c5e996350
userdelroot <r00t316@gmail.com>
2010-10-18 04:47:02 -0400

    delete long click context menu works and list refreshes with dirty hack, probably a better way but the way it is suppose to work does not work

--- src/fac/userdelroot/droidprofiles/ProfilesActivity.java
+++ src/fac/userdelroot/droidprofiles/ProfilesActivity.java
@@ -34,10 +34,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.CursorAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
 /**
  *  
@@ -45,12 +47,14 @@ import android.widget.AdapterView.OnItemClickListener;
  */
 public class ProfilesActivity extends Activity implements OnItemClickListener {
 
+    private static final String TAG = "ProfilesActivity ";
 	private static final int MENU_ACTIVE = Menu.FIRST;
 	private static final int MENU_DEACTIVE = Menu.FIRST + 1;
 	private ListView mList;
-	private SimpleCursorAdapter adapter;
+	private SimpleCursorAdapter mSimpleAdapter;
 	private long mProfileId;
-
+	private Cursor mCursor;
+	
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
@@ -95,61 +99,50 @@ public class ProfilesActivity extends Activity implements OnItemClickListener {
 	/**
 	 * TODO: move this into {@link Profiles} then just return the cursor
 	 */
-	private final void PopulateProfileList() {
+	private final void fillProfileList() {
 
 		Uri uri = Profile.Columns.CONTENT_URI;
-		Cursor cursor;
 		String projection[] = { Profile.Columns.NAME, Profile.Columns._ID };
 		String sortOrder = Profile.Columns.NAME + " COLLATE LOCALIZED ASC";
-		cursor = (Cursor) managedQuery(uri, projection, null, null, sortOrder);
-
+		mCursor = managedQuery(uri, projection, null, null, sortOrder);
 		int to[] = { R.id.profile_list_id, R.id.profile_list_name };
-		adapter = new SimpleCursorAdapter(this, R.layout.profiles_list, cursor,
+		mSimpleAdapter = new SimpleCursorAdapter(this, R.layout.profiles_list, mCursor,
 				projection, to);
-
-		this.mList.setAdapter(adapter);
-
-		// set the lsitener
+		mList.setAdapter(mSimpleAdapter);
+		
+		
+		// set the lsitener(s)
 		mList.setOnItemClickListener(this);
 		mList.setOnCreateContextMenuListener(this);
 		
-/*		mList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-               // delete this profile and everything associated with it
-                Log.v("ID LONG PRESS " + id + " POSITION " + position);
-                
-                return true;
-            }
-        });
-        */
+		if (Log.LOGV)
+		    Log.i(TAG + "fillProfileList() row count " + mCursor.getCount() );
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		adapter = null;
+		mSimpleAdapter = null;
+		//mCursor.close();
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 
-		PopulateProfileList();
+		fillProfileList();
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		// do i need to do this ?
-		adapter = null;
+		mCursor.close();
+		mSimpleAdapter = null;
 		mList = null;
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-	 */
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -160,9 +153,7 @@ public class ProfilesActivity extends Activity implements OnItemClickListener {
 		return true;
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
-	 */
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
@@ -182,8 +173,8 @@ public class ProfilesActivity extends Activity implements OnItemClickListener {
     public boolean onContextItemSelected(MenuItem item) {
 
         final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-
-        final int id = (int) info.id;
+        final long id = info.id;
+        
         switch (item.getItemId()) {
             case R.id.set_profile_active:
                 break;
@@ -199,11 +190,17 @@ public class ProfilesActivity extends Activity implements OnItemClickListener {
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface d, int w) {
 
-                                        Log.i("Delete Profile");
+                                       deleteProfile(id,info.position);
+                                       
                                     }
                                 }).setNegativeButton(android.R.string.cancel, null).show();
                 return true;
+                
+           default:
+              break;
+        
         }
+        
         return super.onContextItemSelected(item);
     }
 	
@@ -213,7 +210,46 @@ public class ProfilesActivity extends Activity implements OnItemClickListener {
         getMenuInflater().inflate(R.menu.context_menu, menu);
 
     }
+    
+    /**
+     * Delete the profile TODO: maybe add a toast with the profile name that was
+     * deleted?
+     * 
+     * @param id
+     */
+    private void deleteProfile(long id, int pos) {
 
+        
+        Profiles.deleteProfile(getContentResolver(), id);
+
+        /*
+         * FIXME: quick and dirty, I have tried many attempts to
+         * mCursor.requery() and adapter.notifyDataSetChanged() to get the
+         * screen to refresh to no avail. 
+         * 
+         * null adapter null cursor and polulate list again.
+         */
+        mSimpleAdapter = null;
+        mCursor.close();
+        mCursor = null;
+
+        Uri uri = Profile.Columns.CONTENT_URI;
+        String projection[] = {
+                Profile.Columns.NAME, Profile.Columns._ID
+        };
+        String sortOrder = Profile.Columns.NAME + " COLLATE LOCALIZED ASC";
+        mCursor = managedQuery(uri, projection, null, null, sortOrder);
+        // startManagingCursor(mCursor);
+        int to[] = {
+                R.id.profile_list_id, R.id.profile_list_name
+        };
+        mSimpleAdapter = new SimpleCursorAdapter(this, R.layout.profiles_list, mCursor, projection,
+                to);
+        mList.setAdapter(mSimpleAdapter);
+
+        Toast.makeText(this, R.string.profile_deleted, Toast.LENGTH_SHORT).show();
+    }
+    
     private void serviceOn(boolean b) {
 		
 		if (b) { 
